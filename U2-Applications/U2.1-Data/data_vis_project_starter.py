22d31814f248335d494b02b85206a40b25a0a548
tmax818 <tylermaxwell661@gmail.com>
2019-07-01 14:51:12 -0700

    amazing data project that was fun

--- U2-Applications/U2.1-Data/data_vis_project_starter.py
+++ U2-Applications/U2.1-Data/data_vis_project_starter.py
@@ -7,6 +7,11 @@ need!
 import json
 from textblob import TextBlob
 import matplotlib.pyplot as plt
+# from wordcloud import WordCloud
+
+# def avg(lst):
+#     return sum(lst)/len(lst)
+
 
 #Get the JSON data
 tweetFile = open("TwitterData/tweets_small.json", "r")
@@ -14,7 +19,46 @@ tweetData = json.load(tweetFile)
 tweetFile.close()
 
 # Continue your program below! 
+tweettext = []
+tweetstring = ""
+for tweet in tweetData:
+    tweetstring += tweet['text']
+    # blob = TextBlob(tweet['text'])
+    #tweettext.append(blob)
+
+#print(tweetstring)
+
+tweetBlob = TextBlob(tweetstring)
+
+print(tweetBlob.translate(to='es'))
+
+blob_polarity = []
+for blob in tweettext:
+    blob_polarity.append(blob.polarity)
+
+blob_subjectivity = []
+for blob in tweettext:
+    blob_subjectivity.append(blob.subjectivity)
+
+word_dict = {}
+
+for word in tweetBlob.words:
+    word_dict[word.lower()] = tweetBlob.word_counts[word.lower()]
+
+print(word_dict) 
+
+# avgPol = avg(blob_polarity)
+# avgSub = avg(blob_subjectivity)
+
+# print(avgPol, avgSub)
+
 
 # Textblob sample:
-tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+# tb = TextBlob("You are a horrible computer scientist.")
+# words = tb.words
+#print(tb.sentiment)
+# print(TextBlob("bad").sentiment)
+
+# for word in words:
+#     print(TextBlob(word).sentiment)
+
